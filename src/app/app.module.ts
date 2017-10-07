24cc9d8fe764775b5ff7c5807880677187efd283
DimaGlushchenko <aws.inspiration@gmail.com>
2017-10-07 20:23:15 +0300

    ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,7 @@ import {
   MatButtonModule,
   MatIconModule
 } from "@angular/material";
-
 import "hammerjs";
-
-import { AngularFireModule } from "angularfire2";
-import { AngularFireDatabaseModule } from "angularfire2/database";
-import { AngularFireAuthModule } from "angularfire2/auth";
-import { environment } from "../environments/environment";
-export const firebaseConfig = environment.firebaseConfig;
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from './ui/header/header.component';
@@ -28,7 +21,7 @@ import { FooterComponent } from "./ui/footer/footer.component";
 import { SidenavComponent } from './ui/sidenav/sidenav.component';
 
 import { PagesModule } from './pages/pages.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -40,19 +33,15 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   imports: [
     BrowserModule.withServerTransition({ appId: "APP_ID" }),
     BrowserAnimationsModule,
-    FlexLayoutModule,
     RoutingModule,
     FormsModule,
     HttpModule,
     HttpClientModule,
     PagesModule,
-
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule,
-    AngularFireDatabaseModule,
     
-    MatToolbarModule, MatIconModule,
-    MatSidenavModule, MatMenuModule, MatButtonModule
+    MatToolbarModule, MatIconModule, 
+    MatSidenavModule, MatMenuModule, 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
