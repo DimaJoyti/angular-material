9142e5aff0ddf230dc5846858e240d274fbb792f
DimaGlushchenko <aws.inspiration@gmail.com>
2017-12-26 14:57:09 +0200

    ready

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,49 +1,69 @@
 import { Component, NgModule } from "@angular/core";
-import { transition, trigger, style, animate, 
-  query, stagger, animateChild } from "@angular/animations";
+import {
+  transition,
+  trigger,
+  style,
+  animate,
+  query,
+  stagger,
+  animateChild
+} from "@angular/animations";
+import { LoginComponent } from "../../pages/login/login.component";
+import { MatDialog } from "@angular/material";
 
 @Component({
-  selector: 'header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  selector: "header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
   animations: [
-    trigger('list', [
-      transition(':enter', [
-        query('@items', stagger(300, animateChild()))
-      ]),
+    trigger("list", [
+      transition(":enter", [query("@items", stagger(300, animateChild()))])
     ]),
-    trigger('items', [
-      transition(':enter', [
-        style({ transform: 'scale(0.5)', opacity: 0 }),  // initial
-        animate('1s cubic-bezier(.8, -0.6, 0.2, 1.5)', 
-          style({ transform: 'scale(1)', opacity: 1 }))  // final
+    trigger("items", [
+      transition(":enter", [
+        style({ transform: "scale(0.5)", opacity: 0 }), // initial
+        animate(
+          "1s cubic-bezier(.8, -0.6, 0.2, 1.5)",
+          style({ transform: "scale(1)", opacity: 1 })
+        ) // final
       ]),
-      transition(':leave', [
-        style({ transform: 'scale(1)', opacity: 1, height: '*' }),
-        animate('1s cubic-bezier(.8, -0.6, 0.2, 1.5)', 
-         style({ 
-           transform: 'scale(0.5)', opacity: 0, 
-           height: '0px', margin: '0px' 
-         })) 
+      transition(":leave", [
+        style({ transform: "scale(1)", opacity: 1, height: "*" }),
+        animate(
+          "1s cubic-bezier(.8, -0.6, 0.2, 1.5)",
+          style({
+            transform: "scale(0.5)",
+            opacity: 0,
+            height: "0px",
+            margin: "0px"
+          })
+        )
       ])
-    ]),
+    ])
   ]
 })
-export class HeaderComponent{
-  otherTheme: boolean = false; 
+export class HeaderComponent {
+  otherTheme: boolean = false;
   counter = 5;
   list = [];
-  
-  add(){
+
+  constructor(public dialog: MatDialog){}
+
+  add() {
     this.list.push(this.counter++);
   }
 
   remove(index) {
-    if(!this.list.length) return;
+    if (!this.list.length) return;
     this.list.splice(index, 1);
   }
 
   changeTheme() {
     this.otherTheme = !this.otherTheme;
+  }
+
+  openModal() {
+    this.dialog.open(LoginComponent, { 
+      data: { name: "Angular Material" } });
   }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private matDialogRef: MatDialogRef<LoginComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public close() {
+    this.matDialogRef.close();
   }
-
 }
